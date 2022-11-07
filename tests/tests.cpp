fb484c9fad88f5660242bc04a173e65f3ae1fd61
jatinchowdhury18 <jatinchowdhury18@gmail.com>
2022-11-06 20:08:11 -0800

    Add C++17-compatible implementations of bind_front and bind_back (#12)

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -488,6 +488,57 @@ TEST_CASE("Erase")
     }
 }
 
+TEST_CASE("Bindings")
+{
+
+    struct Point
+    {
+        int x = 0, y = 0;
+
+        void displace(int x_off, int y_off)
+        {
+            x += x_off;
+            y += y_off;
+        }
+    };
+
+    SECTION("Bind Front Test")
+    {
+        const auto sum_func = [](int a, int b, int c, int d) { return a + b + c + d; };
+        const auto sum_two = sst::cpputils::bind_front(sum_func, 1, 2);
+        REQUIRE(sum_two(3, 4) == 10);
+    }
+
+    SECTION("Bind Front Struct Test")
+    {
+        Point p{};
+        auto displace = sst::cpputils::bind_front(&Point::displace, &p);
+
+        displace(3, 4);
+
+        REQUIRE(p.x == 3);
+        REQUIRE(p.y == 4);
+    }
+
+    SECTION("Bind Back Test")
+    {
+        const auto func = [](int a, int b, int c, int d) { return a + b - c - d; };
+        const auto sum_two = sst::cpputils::bind_back(func, 1, 2);
+        REQUIRE(sum_two(3, 4) == 4);
+    }
+
+    SECTION("Bind Back Struct Test")
+    {
+        auto displace_3_4 = sst::cpputils::bind_back(&Point::displace, 3, 4);
+
+        Point p{};
+        displace_3_4(&p);
+
+        REQUIRE(p.x == 3);
+        REQUIRE(p.y == 4);
+    }
+}
+
 int main(int argc, char **argv)
 {
     int result = Catch::Session().run(argc, argv);
